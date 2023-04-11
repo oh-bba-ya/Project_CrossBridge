785ba3fa5f6f18807e7921fb92b41fb5182464d6
myeongseok <audtjrauddn@khu.ac.kr>
2023-04-11 16:01:01 +0900

    Create : LobbyGamemode class
    PostLogin 함수를 통해 lobby에 2명의 플레이어가 들어온다면 자동으로 Transition map을 거쳐 실제 게임 레벨로 이동할 수 있도록 했다.

---
+++ Source/Project_CrossBridge/Private/LobbyGameMode.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameState.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	// Player의 숫자를 저장한다.
+	int32 NumberofPlayers = GameState.Get()->PlayerArray.Num();
+
+	if(NumberofPlayers == 2)
+	{
+		UWorld* World = GetWorld();
+		if(World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/Bridge?listen"));
+		}
+	}
+	
+}
